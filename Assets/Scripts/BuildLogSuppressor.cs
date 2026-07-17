80cc3a1e01540ff2a606f05ce9b03907f80c6e2f
F1owerR1117 <a2858701729@gmail.com>
2026-07-17 21:45:13 +0800

    fix: 消除 Object 歧义 + 补全 Log(LogType,object,Object) 方法

--- Assets/Scripts/BuildLogSuppressor.cs
+++ Assets/Scripts/BuildLogSuppressor.cs
@@ -24,24 +24,25 @@ public static class BuildLogSuppressor
 
         public void Log(LogType logType, object message) { }
         public void Log(LogType logType, string tag, object message) { }
-        public void Log(LogType logType, string tag, object message, Object context) { }
+        public void Log(LogType logType, string tag, object message, UnityEngine.Object context) { }
+        public void Log(LogType logType, object message, UnityEngine.Object context) { }
         public void Log(object message) { }
         public void Log(string tag, object message) { }
         public void LogWarning(string tag, object message) { }
-        public void LogWarning(string tag, object message, Object context) { }
+        public void LogWarning(string tag, object message, UnityEngine.Object context) { }
         public void LogError(string tag, object message) { }
-        public void LogError(string tag, object message, Object context) { }
+        public void LogError(string tag, object message, UnityEngine.Object context) { }
         public void LogFormat(LogType logType, string format, params object[] args) { }
-        public void LogFormat(LogType logType, Object context, string format, params object[] args) { }
+        public void LogFormat(LogType logType, UnityEngine.Object context, string format, params object[] args) { }
         public void LogException(Exception exception) { }
-        public void LogException(Exception exception, Object context) { }
+        public void LogException(Exception exception, UnityEngine.Object context) { }
         public bool IsLogTypeAllowed(LogType logType) => false;
     }
 
     private class NullHandler : ILogHandler
     {
         public static readonly NullHandler Instance = new NullHandler();
-        public void LogFormat(LogType logType, Object context, string format, params object[] args) { }
-        public void LogException(Exception exception, Object context) { }
+        public void LogFormat(LogType logType, UnityEngine.Object context, string format, params object[] args) { }
+        public void LogException(Exception exception, UnityEngine.Object context) { }
     }
 }
