bd1774b5a7490e87ecc02e95be470cfb75380c99
F1owerR1117 <a2858701729@gmail.com>
2026-07-17 21:49:46 +0800

    fix: 改用 logEnabled=false + NullHandler 抑制日志（Debug.unityLogger 只读）

--- Assets/Scripts/BuildLogSuppressor.cs
+++ Assets/Scripts/BuildLogSuppressor.cs
@@ -11,39 +11,14 @@ public static class BuildLogSuppressor
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void Init()
     {
-        Debug.unityLogger = new QuietLogger();
-    }
-#endif
-
-    private class QuietLogger : ILogger
-    {
-        public LogType filterLogType { get; set; } = LogType.Exception;
-        public bool logEnabled { get; set; } = false;
-        public bool logHandlerEnabled { get; set; } = false;
-        public ILogHandler logHandler { get; set; } = NullHandler.Instance;
-
-        public void Log(LogType logType, object message) { }
-        public void Log(LogType logType, string tag, object message) { }
-        public void Log(LogType logType, string tag, object message, UnityEngine.Object context) { }
-        public void Log(LogType logType, object message, UnityEngine.Object context) { }
-        public void Log(object message) { }
-        public void Log(string tag, object message) { }
-        public void Log(string tag, object message, UnityEngine.Object context) { }
-        public void LogWarning(string tag, object message) { }
-        public void LogWarning(string tag, object message, UnityEngine.Object context) { }
-        public void LogError(string tag, object message) { }
-        public void LogError(string tag, object message, UnityEngine.Object context) { }
-        public void LogFormat(LogType logType, string format, params object[] args) { }
-        public void LogFormat(LogType logType, UnityEngine.Object context, string format, params object[] args) { }
-        public void LogException(Exception exception) { }
-        public void LogException(Exception exception, UnityEngine.Object context) { }
-        public bool IsLogTypeAllowed(LogType logType) => false;
+        Debug.unityLogger.logEnabled = false;
+        Debug.unityLogger.logHandler = new NullHandler();
     }
 
     private class NullHandler : ILogHandler
     {
-        public static readonly NullHandler Instance = new NullHandler();
         public void LogFormat(LogType logType, UnityEngine.Object context, string format, params object[] args) { }
         public void LogException(Exception exception, UnityEngine.Object context) { }
     }
+#endif
 }
