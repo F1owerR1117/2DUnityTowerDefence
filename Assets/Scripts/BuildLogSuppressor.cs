b0434b9aa3249bea558347bfc44ac138fada5869
F1owerR1117 <a2858701729@gmail.com>
2026-07-17 21:46:08 +0800

    fix: 补全 Log(string,object,Object) 方法

--- Assets/Scripts/BuildLogSuppressor.cs
+++ Assets/Scripts/BuildLogSuppressor.cs
@@ -28,6 +28,7 @@ public static class BuildLogSuppressor
         public void Log(LogType logType, object message, UnityEngine.Object context) { }
         public void Log(object message) { }
         public void Log(string tag, object message) { }
+        public void Log(string tag, object message, UnityEngine.Object context) { }
         public void LogWarning(string tag, object message) { }
         public void LogWarning(string tag, object message, UnityEngine.Object context) { }
         public void LogError(string tag, object message) { }
